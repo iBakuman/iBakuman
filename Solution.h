4031c326fc7e6dcf0e687592c7285e5685ba92de
iBakuman <avarice_global@qq.com>
2021-03-22 12:35:08 +0800

    information

---
+++ Solution.h
@@ -0,0 +1,43 @@
+﻿#include <bits/stdc++.h>
+
+using namespace std;
+
+// class Solution {
+// public:
+// 	void setZeroes (vector<vector<int>>& matrix) {
+// 		vector<bool> recordR(matrix.size(), false);
+// 		vector<bool> recordC(matrix[0].size(), false);
+// 		for (int i = 0; i < matrix.size(); ++i) {
+// 			for (int j = 0; j < matrix[i].size(); ++j) {
+// 				if (matrix[i][j] == 0)
+// 					recordC[j] = recordR[i] = true;
+// 			}
+// 		}
+// 	}
+//
+// 	void fill (vector<vector<int>>& matrix, int row, int column) {
+// 		if (row >= 0 && row < matrix.size()) {
+// 			for (int i         = 0; i < matrix[row].size(); ++i)
+// 				matrix[row][i] = 0;
+// 		}
+//
+// 		if (column >= 0 && column < matrix[0].size()) {
+// 			for (int i            = 0; i < matrix.size(); ++i)
+// 				matrix[i][column] = 0;
+// 		}
+//
+// 	}
+// };
+
+class Solution {
+public:
+	int hammingWeight(uint32_t n) {
+		int res = 0;
+		while(n) {
+			n = n & (n - 1);
+			res++;
+		}
+		return  10;
+		return res;
+	}
+};
---
+++ test.cpp
@@ -0,0 +1,13 @@
+﻿#include "pch.h"
+
+TEST(TestCaseName, TestName) {
+
+	using namespace  std;
+
+	char* arr = "Hello world";
+	cout << strlen(arr) << endl;
+	
+
+	EXPECT_EQ(1, 1);
+	EXPECT_TRUE(true);
+}
