6f9009c74f7813951efc83d4ca2923b95b0454a6
iBakuman <90392644@qq.com>
2021-03-23 19:11:32 +0800

    infomation

--- Solution.h
+++ Solution.h
@@ -29,19 +29,29 @@ using namespace std;
 // 	}
 // };
 
-class Solution {
-public:
-	int hammingWeight(uint32_t n) {
-		int res = 0;
-		while(n) {
-			n = n & (n - 1);
-			res++;
-		}
-		cout << "hello world" << endl;
-		cout << "C++" << endl;
-		cout << "changes 1" << endl;
-		cout << "changes 3" << endl;
-		cout << "changes 4 in hot_fix" << endl;
-		return res;
-	}
-};
+/**
+ * // This is the interface that allows for creating nested lists.
+ * // You should not implement it, or speculate about its implementation
+ * class NestedInteger {
+ *   public:
+ *     // Return true if this NestedInteger holds a single integer, rather than a nested list.
+ *     bool isInteger() const;
+ *
+ *     // Return the single integer that this NestedInteger holds, if it holds a single integer
+ *     // The result is undefined if this NestedInteger holds a nested list
+ *     int getInteger() const;
+ *
+ *     // Return the nested list that this NestedInteger holds, if it holds a nested list
+ *     // The result is undefined if this NestedInteger holds a single integer
+ *     const vector<NestedInteger> &getList() const;
+ * };
+ */
+
+
+
+
+/**
+ * Your NestedIterator object will be instantiated and called as such:
+ * NestedIterator i(nestedList);
+ * while (i.hasNext()) cout << i.next();
+ */
